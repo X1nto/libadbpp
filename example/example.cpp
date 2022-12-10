964b3510cdee35cd9dc02fb24282acf158f3bb1d
X1nto <burtkni@yandex.ru>
2022-12-10 17:59:28 +0400

    update example

--- example/example.cpp
+++ example/example.cpp
@@ -1,26 +1,26 @@
-//#define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 
 #include <iostream>
 #include <string>
 #include <sstream>
-//#include <windows.h>
-//#include <winusb.h>
-//#include <WinSock2.h>
+#include <windows.h>
+#include <WinSock2.h>
 #include "adb/connection.h"
 #include "adb/transport.h"
 #include "adb/protocol/protocol.h"
 #include "adb/protocol/serialization.h"
 #include "adb/protocol/util.h"
 
-//#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")
 
-//#define ABORT(MESSAGE)                                                   \
-//              std::cout << MESSAGE << ": " << WSAGetLastError() << '\n'; \
-//              closesocket(adb_socket);                                   \
-//              WSACleanup();                                              \
-//              return -1;                                                 \
+#define ABORT(MESSAGE)                                                   \
+              std::cout << MESSAGE << ": " << WSAGetLastError() << '\n'; \
+              closesocket(adb_socket);                                   \
+              WSACleanup();                                              \
+              return -1;                                                 \
 
 using adb_packet = adb::protocol::adb_packet;
+using adb_message = adb::protocol::adb_message;
 constexpr auto make_connect_packet = adb::protocol::make_connect_packet;
 constexpr auto systype_host = adb::protocol::adb_system_identity_systype::host;
 constexpr auto serialize = adb::protocol::serializer::serialize;
@@ -29,33 +29,24 @@ constexpr auto deserialize = adb::protocol::serializer::deserialize;
 int main()
 {
 	const adb_packet packet = make_connect_packet(systype_host, "fake-adb", "real-adb");
+	const size_t packet_size = sizeof(adb_message) + packet.payload.size();
 
-	std::string serialized = serialize(packet);
-	adb_packet deserialized = deserialize(serialized);
-
-	std::cout << "Initial: " << packet << '\n';
-	std::cout << "Serialized: " << serialized << '\n';
-	std::cout << "Deserialized: " << deserialized << '\n';
-
-	/*WSAData wsadata;
+	WSAData wsadata;
 	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != NO_ERROR)
 	{
-		std::cout << "failed to initialize wsa" << '\n';
+		std::cout << "Failed to initialize WSA: " << WSAGetLastError() << '\n';
 		return -1;
 	}
 
-	std::string::npos;
-
 	SOCKET adb_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	sockaddr_in address {.sin_family = AF_INET, .sin_port = htons(42261)};
-	address.sin_addr.s_addr = inet_addr("192.168.100.3");
+	sockaddr_in address {.sin_family = AF_INET, .sin_port = htons(37503)}; //replace with the device's ADB port
+	address.sin_addr.s_addr = inet_addr("192.168.100.3"); //replace with the device's ADB IP
 	if (connect(adb_socket, (SOCKADDR*) &address, sizeof(address)) == SOCKET_ERROR)
 	{
 		ABORT("Could not connect")
 	}
 
-	std::cout << "Outgoing: " << packet.as_string() << '\n';
-	if (send(adb_socket, packet.as_string().c_str(), packet.size(), 0) == SOCKET_ERROR)
+	if (send(adb_socket, serialize(packet).c_str(), packet_size, 0) == SOCKET_ERROR)
 	{
 		ABORT("Send failed")
 	};
@@ -67,7 +58,8 @@ int main()
 
 		if (receive_status > 0)
 		{
-			std::cout << "Incoming: " << incoming_buf << '\n';
+			adb_packet deserialized = deserialize(incoming_buf);
+			std::cout << "Incoming: " << deserialized << '\n';
 		} else if (receive_status == 0)
 		{
 			std::cout << "Connection closed" << '\n';
@@ -85,7 +77,7 @@ int main()
 	}
 
 	closesocket(adb_socket);
-	WSACleanup();*/
+	WSACleanup();
 
 	return 0;
 }
